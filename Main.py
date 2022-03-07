bf433557adea1947a6d0cf7b73644c359126161e
klow358 <klow358@aucklanduni.ac.nz>
2022-03-07 23:47:54 +1300

    Initial commit

---
+++ Main.py
@@ -0,0 +1,33 @@
+import pyautogui,pyaudio,speech_recognition as sr, time
+screenWidth, screenLength = pyautogui.size()
+#print(screenWidth, screenLength)
+
+
+'''
+def Carousel():
+
+def Buy():
+
+def Sell():
+
+def Place():
+
+def Orb():
+
+def Item():
+
+def Augement():
+
+def Level():
+
+def Roll():
+
+def Scout():
+
+def Lock():
+
+def Feed():
+'''
+def main():
+
+main()

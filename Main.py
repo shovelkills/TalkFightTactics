a7cbb34f9d169ebc98b16c8bf788c2a7d18e3541
shovelkills <shovelkills@gmail.com>
2022-05-04 23:38:16 +1200

    Created some code to read a excel sheet of ratios

--- Main.py
+++ Main.py
@@ -1,58 +1,60 @@
-import pyautogui,pyaudio,speech_recognition as sr, time
-screenWidth, screenLength = pyautogui.size()
-print(screenWidth, screenLength)
-
-class ItemPositions:
-    xItem = []
-    yItem = []
-    @classmethod
-    def
-class boardPositions:
-    xBoard = []
-    yBoard = []
-    @classmethod
-
-class buyPositions:
-    xBuy = []
-    yBuy = []
-    @classmethod
-
-class benchPositions:
-    xBench = []
-    yBench = []
-    @classmethod
-
-class actions:
+import pyautogui,pyaudio,speech_recognition as sr, time, pandas as pd
+screenwidth, screenlength = pyautogui.size()
+print(screenwidth, screenlength)
 
 
-#def Carousel():
-
-def Buy():
-
-def Sell():
-
-#def Place():
-
-#def Orb():
-
-def Item():
-
-def Augement():
-
-def Level():
-
-def Roll():
-
-#def Scout():
-
-def Lock():
-
+# class ItemPositions:
+#     xItem = []
+#     yItem = []
+#     @classmethod
+#     def
+#
+# class boardPositions:
+#     xBoard = []
+#     yBoard = []
+#     @classmethod
+#
+# class buyPositions:
+#     xBuy = []
+#     yBuy = []
+#     @classmethod
+#
+# class benchPositions:
+#     xBench = []
+#     yBench = []
+#     @classmethod
+#
+# class actions:
+#
+#
+# #def Carousel():
+#
+# def Buy():
+#
+# def Sell():
+#
+# #def Place():
+#
+# #def Orb():
+#
+# def Item():
+#
+# def Augement():
+#
+# def Level():
+#
+# def Roll():
+#
+# #def Scout():
+#
+# def Lock():
 
 
 
 def main():
-
-
+    data = pd.read_excel(r'TFTRatios.xlsx')
+    df = pd.DataFrame(data, columns=['X Ratio', 'Y Ratio'])
+    print (df)
 
 if __name__ == "__main__":
     main()

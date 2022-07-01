2572ddd065deac1a923a9e475d672305cb9c46eb
shovelkills <shovelkills@gmail.com>
2022-07-02 00:50:29 +1200

    Rearranged the code to make it more efficient

--- Main.py
+++ Main.py
@@ -1,60 +1,52 @@
+from abc import abstractmethod
+
 import pyautogui,pyaudio,speech_recognition as sr, time, pandas as pd
-screenwidth, screenlength = pyautogui.size()
-print(screenwidth, screenlength)
+
+#Global Variables
+screenwidth = 0
+screenlength = 0
 
 
-# class ItemPositions:
-#     xItem = []
-#     yItem = []
-#     @classmethod
-#     def
-#
-# class boardPositions:
-#     xBoard = []
-#     yBoard = []
-#     @classmethod
-#
-# class buyPositions:
-#     xBuy = []
-#     yBuy = []
-#     @classmethod
-#
-# class benchPositions:
-#     xBench = []
-#     yBench = []
-#     @classmethod
-#
-# class actions:
-#
-#
-# #def Carousel():
-#
-# def Buy():
-#
-# def Sell():
-#
-# #def Place():
-#
-# #def Orb():
-#
-# def Item():
-#
-# def Augement():
-#
-# def Level():
-#
-# def Roll():
-#
-# #def Scout():
-#
-# def Lock():
+class Positions:
+    xBoard = {} # 0 - 27, A = 0 - 6, B = 7 - 13, c = 14 - 20, d = 21 - 27
+    yBoard = {}
+    @abstractmethod
+    def __init__(self):
+       pass
+
+    def addBoard(self, name, xValue, yValue):
+        self.xBoard[name] = xValue
+        self.yBoard[name] = yValue
+
+class ItemClass(Positions): #28-37
+    @classmethod
+
+class BenchClass(Positions):#38 - 45
+    @classmethod
+
+class ShopClass(Positions):
+    #Actions: Augement, Armory, Radiant Armory, Tome, Buy
+    @classmethod
+
+class ActionsClass(Positions):
+    #Action: Lock, level, Roll, Sell,
+
+def moveAndClick(xValue, yValue):
+
+def dragAndClick(xInitial, yInitial, xFinal, yFinal):
+
 
 
 
 def main():
+    screenwidth, screenlength = pyautogui.size()
     data = pd.read_excel(r'TFTRatios.xlsx')
-    df = pd.DataFrame(data, columns=['X Ratio', 'Y Ratio'])
-    print (df)
+    df = pd.DataFrame(data, columns=['Position', 'X Ratio', 'Y Ratio'])  #Gets x and y ratios
+    df["X Ratio"] = screenwidth * df["X Ratio"]
+    df["Y Ratio"] = screenlength * df["Y Ratio"]
+    print(df)
+    item, bench, shop , actions = ItemClass(), BenchClass(), ShopClass(), ActionsClass()
+
 
 if __name__ == "__main__":
     main()

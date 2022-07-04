a3f1bbf69b030fbfa6a0be8d91e98ebc7fed5166
shovelkills <shovelkills@gmail.com>
2022-07-04 16:35:20 +1200

    Made it so it adds all the values to the respective classes

--- Main.py
+++ Main.py
@@ -1,52 +1,70 @@
 from abc import abstractmethod
 
-import pyautogui,pyaudio,speech_recognition as sr, time, pandas as pd
+import pyautogui as pygui,pyaudio,speech_recognition as sr, time, pandas as pd
 
 #Global Variables
-screenwidth = 0
-screenlength = 0
+screenwidth, screenlength = pygui.size()
 
 
 class Positions:
-    xBoard = {} # 0 - 27, A = 0 - 6, B = 7 - 13, c = 14 - 20, d = 21 - 27
-    yBoard = {}
+
     @abstractmethod
     def __init__(self):
-       pass
-
-    def addBoard(self, name, xValue, yValue):
-        self.xBoard[name] = xValue
-        self.yBoard[name] = yValue
-
-class ItemClass(Positions): #28-37
+        self.xPosition = {}
+        self.yPosition = {}
     @classmethod
+    def addPosition(self, name, xvalue, yvalue):#Adds to position dictionaries
+        self.xPosition[name] = xvalue
+        self.yPosition[name] = yvalue
+    def showPosition(self):
+        print(self.yPosition)
+        print(self.xPosition)
 
-class BenchClass(Positions):#38 - 45
-    @classmethod
+class BoardClass(Positions):# 0 - 27, A = 0 - 6, B = 7 - 13, c = 14 - 20, d = 21 - 27
+    #Actions: Move from board to board and bench
+    #Action:Move item from bench to unit - bench and board
+    #Action: Move from bench to board and bench
+    xPosition = {}
+    yPosition = {}
+    @abstractmethod
+    def __init__(self):
+        super()
+    def addPosition(self, name, xvalue, yvalue):
+        super().addPosition(name, xvalue, yvalue)
 
 class ShopClass(Positions):
-    #Actions: Augement, Armory, Radiant Armory, Tome, Buy
-    @classmethod
-
-class ActionsClass(Positions):
+    #Actions: Pick Augement, Armory, Radiant Armory, Tome, Buy
     #Action: Lock, level, Roll, Sell,
+    xPosition = {}
+    yPosition = {}
+    @abstractmethod
+    def __init__(self):
+        super()
+    def addPosition(self, name, xvalue, yvalue):
+        super().addPosition(name, xvalue, yvalue)
 
-def moveAndClick(xValue, yValue):
+def moveAndClick(xvalue, yvalue):
+    pygui.click(x=xvalue, y=yvalue, button ='left')
 
-def dragAndClick(xInitial, yInitial, xFinal, yFinal):
-
+def dragAndClick(xinitial, yinitial, xfinal, yfinal):
+    pygui.mouseDown(x = xinitial, y = yinitial)
+    pygui.mouseUp(x = xfinal, y = yfinal)
 
 
 
 def main():
-    screenwidth, screenlength = pyautogui.size()
     data = pd.read_excel(r'TFTRatios.xlsx')
     df = pd.DataFrame(data, columns=['Position', 'X Ratio', 'Y Ratio'])  #Gets x and y ratios
     df["X Ratio"] = screenwidth * df["X Ratio"]
     df["Y Ratio"] = screenlength * df["Y Ratio"]
-    print(df)
-    item, bench, shop , actions = ItemClass(), BenchClass(), ShopClass(), ActionsClass()
-
-
+    board, shop = BoardClass(), ShopClass()
+    for i in range(len(df)):
+        temp = df.iloc[i]
+        if ("Item" in temp['Position'] or "Bench" in temp['Position'] or len(temp['Position']) == 2):
+            board.addPosition(temp['Position'], temp['X Ratio'], temp['Y Ratio'])
+        else:
+            shop.addPosition(temp['Position'], temp['X Ratio'], temp['Y Ratio'])
+    board.showPosition()
+    shop.showPosition()
 if __name__ == "__main__":
     main()
